77da5ac182202f686c23fae9b1e2f8ae6e6cf1fd
Eng-AhmedKamel1 <ahmedkamela335@gmail.com>
2025-10-26 09:18:13 +0300

    Final: Adjusted relative paths in index.html and rebuilt dist

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Ahmed-Kamel-Portfolio/', // يجب أن يكون موجوداً بهذا الشكل
+  // تأكد من وجود هذا السطر
+  base: '/Ahmed-Kamel-Portfolio/', 
 })
