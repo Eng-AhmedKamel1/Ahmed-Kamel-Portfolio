75fa2ab13f8a8e4966f6331cf2bab1bfa3397a55
Eng-AhmedKamel1 <ahmedkamela335@gmail.com>
2025-10-26 09:11:53 +0300

    Fixing asset path in index.html for relative loading

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
 // vite.config.js
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// تأكد أن هذا الاسم مطابق تماماً لاسم مستودعك على GitHub
-const repoName = 'Ahmed-Kamel-Portfolio'; 
-
 export default defineConfig({
   plugins: [react()],
-  
-  // يتم إضافة خاصية base هنا لتصحيح المسارات على GitHub Pages
-  base: `/${repoName}/`, 
+  base: '/Ahmed-Kamel-Portfolio/', // يجب أن يكون موجوداً بهذا الشكل
 })
