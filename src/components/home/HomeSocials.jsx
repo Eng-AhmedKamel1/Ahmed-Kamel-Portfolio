17942960d2d480d26b98431c528e74e5ce3cff4e
Eng-AhmedKamel1 <ahmedkamela335@gmail.com>
2025-10-26 16:41:52 +0300

    Update social links in HomeSocials component

--- src/components/home/HomeSocials.jsx
+++ src/components/home/HomeSocials.jsx
@@ -6,8 +6,8 @@ import { FaDribbble } from "react-icons/fa6";
 function HomeSocials() {
   return (
     <div className="home-socials">
-        <a href="" target='_blank'><FaLinkedin /></a>
-        <a href="" target='_blank'><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/ahmed-kamel-996aa0271/" target='_blank'><FaLinkedin /></a>
+        <a href="https://github.com/Eng-AhmedKamel1" target='_blank'><FaGithub /></a>
         <a href="" target='_blank'><FaDribbble /></a>
     </div>
   )
